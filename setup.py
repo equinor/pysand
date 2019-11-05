8a0ece682b4f282979fd17fc62664dbf113c0566
Thorjan Knudsvik <t@knudsvik.com>
2019-11-05 22:55:51 +0100

    Initial public version

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='pysand',
       author='Thorjan Knudsvik',
       author_email='thokn@equinor.com',
       url='https://github.com/equinor/pysand',
+      download_url='https://github.com/equinor/pysand/archive/1.0.tar.gz',
       packages=['pysand'],
       install_requires=['numpy', 'scipy']
 )
