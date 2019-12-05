00e4801d8a1f10c2bb6337db8d006b8fa00f7aad
Alisher Narzulloyev <anarz@equinor.com>
2019-12-05 15:55:42 +0100

    bug fix in test_validate_fluid_props

--- tests/test_fluidproperties.py
+++ tests/test_fluidproperties.py
@@ -4,14 +4,15 @@ from pysand.fluidproperties import mix_velocity, mix_density, mix_viscosity, val
 
 
 def test_validate_fluid_props():
-    # Testing non-number input throws exception
-    kwargs = {'P': 1, 'T': 1, 'Qo': 1, 'Qw': 1, 'Qg': 1, 'Z': 1, 'D': 1, 'rho_o': 1, 'rho_w': 1, 'MW': 1,
-              'mu_o': 1, 'mu_w': 1, 'mu_g': 1}
+    num = 1
+    kwargs = {'P': num, 'T': num, 'Qo': num, 'Qw': num, 'Qg': num, 'Z': num, 'D': num, 'rho_o': num, 'rho_w': num,
+              'MW': num, 'mu_o': num, 'mu_w': num, 'mu_g': num}
     for inp in ['P', 'T', 'Qo', 'Qw', 'Qg', 'Z', 'D', 'rho_o', 'rho_w', 'MW', 'mu_o', 'mu_w', 'mu_g']:
         for non_number in [None, 'string']:
             kwargs[inp] = non_number
             with pytest.raises(exc.FunctionInputFail) as excinfo:
                 validate_fluid_props(**kwargs)
+        kwargs[inp] = num
 
 
 velocity_validation = [(10, 40, 1200, 3000, 100000, .95, .15, 9.58)]
