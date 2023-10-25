1adb9fcdfeae2a62c60a26740e53b82ab8fbceef
smolvik1 <smolvik@hotmail.com>
2023-10-25 21:49:28 +0200

    chore: improved readability

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 import pathlib
 from setuptools import setup
 
-exec(open('pysand/version.py').read())
 
-HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").read_text()
+def get_version():
+    with open('pysand/version.py') as version_file:
+        namespace = {}
+        exec(version_file.read(), namespace)
+        return namespace['__version__']
 
-setup(name='pysand',
-      version=__version__,
-      description='Sand management related calculations',
-      long_description=README,
-      long_description_content_type="text/markdown",
-      author='Equinor ASA',
-      author_email='stimo@equinor.com',
-      license='GNU GPL',
-      url='https://github.com/equinor/pysand',
-      classifiers=["Programming Language :: Python :: 3"],
-      packages=['pysand'],
-      install_requires=['pandas', 'numpy>=1.17', 'scipy']
-)
+VERSION = get_version()
+
+# Read the README.md file
+home_dir = pathlib.Path(__file__).parent
+README = (home_dir / "README.md").read_text()
+
+if __name__ == "__main__":
+      setup(
+            name='pysand',
+            version=VERSION,
+            description='Sand management related calculations',
+            long_description=README,
+            long_description_content_type="text/markdown",
+            author='Equinor ASA',
+            author_email='stimo@equinor.com',
+            license='GNU GPL',
+            url='https://github.com/equinor/pysand',
+            classifiers=["Programming Language :: Python :: 3"],
+            packages=['pysand'],
+            install_requires=['pandas', 'numpy>=1.17', 'scipy']
+      )
